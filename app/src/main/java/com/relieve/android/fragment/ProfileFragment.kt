bc33161fd7c0576cf97d17175e9dacf420afec79
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-11-22 00:15:31 +0700

    fragments switch

---
+++ app/src/main/java/com/relieve/android/fragment/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.relieve.android.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.relieve.android.R
+import kotlinx.android.synthetic.main.recycler_view_full.view.*
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.recycler_view_full, container, false).apply {
+            //            adapter = Adapter(context)
+            this.rvHome.layoutManager = LinearLayoutManager(context)
+//            rvHome.adapter = adapter
+        }
+    }
+}
