0e298f0fd7ae2917327464f802cb8ec43cac0aea
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-11-21 19:18:36 +0700

    working navbar

--- app/src/main/java/com/relieve/android/MainActivity.kt
+++ app/src/main/java/com/relieve/android/MainActivity.kt
@@ -2,11 +2,35 @@ package com.relieve.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.relieve.android.components.BottomNavBar
+import kotlinx.android.synthetic.main.view_bottom_nav.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindNavClick()
+    }
+
+    private fun bindNavClick() {
+        BottomNavBar(bottomBar).apply {
+            setHomeClickListener {
+
+            }
+            setDiscoverClickListener {
+
+            }
+            setCallClickListener {
+
+            }
+            setChatClickListener {
+
+            }
+            setProfileClickListener {
+
+            }
+        }
     }
 }
