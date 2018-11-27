e96fd07636ddc1bc640798851b255d53a7d08693
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-11-28 01:06:51 +0700

    walkthough

--- app/src/main/java/com/relieve/android/fragment/boarding/BoardingRegisterFragment.kt
+++ app/src/main/java/com/relieve/android/fragment/boarding/BoardingRegisterFragment.kt
@@ -7,8 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.relieve.android.R
-import kotlinx.android.synthetic.main.activity_boarding.*
 import kotlinx.android.synthetic.main.fragment_boarding_register.*
 
 class BoardingRegisterFragment : Fragment() {
@@ -34,5 +34,8 @@ class BoardingRegisterFragment : Fragment() {
     }
 
     private fun render() {
+        tvRegister.setOnClickListener {
+            findNavController().navigate(R.id.walkthroughFragment)
+        }
     }
 }
