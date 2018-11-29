55d751e9b40687aaf220743d39be350cac211e0c
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-11-29 19:05:04 +0700

    request login

--- app/src/main/java/com/relieve/android/viewmodel/boarding/BoardingViewModel.kt
+++ app/src/main/java/com/relieve/android/viewmodel/boarding/BoardingViewModel.kt
@@ -4,10 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.relieve.android.network.service.RelieveService
 import com.relieve.android.network.data.relieve.Login
 import com.relieve.android.network.data.relieve.Register
+import com.relieve.android.network.isRequestSuccess
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.disposables.CompositeDisposable
+
+
 
 class BoardingViewModel : ViewModel() {
+
+    val compositeDisposable = CompositeDisposable()
 
     private val relieveService by lazy {
         RelieveService.create()
@@ -17,10 +23,10 @@ class BoardingViewModel : ViewModel() {
         relieveService.login(Login(password = password, username = username))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe (
-                        { result -> onResponse(true) },
-                        { error -> onResponse(false) }
-                )
+            .subscribe(
+                { result -> onResponse(result.status?.isRequestSuccess() == true) },
+                { onResponse(false) }
+            ).also { compositeDisposable.add(it) }
     }
 
     fun registerClick(userData: Register, onResponse: (Boolean) -> Unit) {
@@ -30,10 +36,14 @@ class BoardingViewModel : ViewModel() {
             .subscribe(
                 { result -> onResponse(true) },
                 { error -> onResponse(false) }
-            )
+            ).also { compositeDisposable.add(it) }
     }
 
     fun onGoogleLogin(idToken: String, onResponse: (Boolean) -> Unit) {
         onResponse(true)
+    }
+
+    fun onDestroy () {
+        compositeDisposable.dispose()
     }
 }
