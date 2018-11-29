e6e2472033dfddbc6a592483ce1b2b4833ab0bb8
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-11-30 02:07:45 +0700

    create event req

---
+++ app/src/main/java/com/relieve/android/network/data/camar/Event.kt
@@ -0,0 +1,26 @@
+package com.relieve.android.network.data.camar
+
+import com.google.gson.annotations.SerializedName
+
+data class Event(
+    @field:SerializedName("id")
+    val id: String? = null,
+
+    @field:SerializedName("title")
+    val title: String? = null,
+
+    @field:SerializedName("mag")
+    val location: Location? = null,
+
+    @field:SerializedName("mag")
+    val magnitude: Double? = null,
+
+    @field:SerializedName("depth")
+    val depth: Double? = null,
+
+    @field:SerializedName("place")
+    val place: String? = null,
+
+    @field:SerializedName("time")
+    val time: Long? = null
+)
--- app/src/main/java/com/relieve/android/network/service/CamarService.kt
+++ app/src/main/java/com/relieve/android/network/service/CamarService.kt
@@ -5,6 +5,7 @@ import com.relieve.android.BuildConfig
 import com.relieve.android.network.Camar
 import com.relieve.android.network.data.camar.ApiResponse
 import com.relieve.android.network.data.camar.Device
+import com.relieve.android.network.data.camar.Event
 import io.reactivex.Observable
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,12 +13,17 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface CamarService {
 
     @POST("device")
     fun postNewDevice(@Body device: Device): Observable<ApiResponse<Device>>
+
+    @GET("earthquakeList")
+    fun postNewDevice(@Query("limit") limit: Int, @Query("page") page: Int): Observable<ApiResponse<Event>>
 
     companion object {
         fun create(): CamarService {
