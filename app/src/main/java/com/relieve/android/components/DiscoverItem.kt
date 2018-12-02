f08f892f9e18e5d2814f740862b2939079dad463
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-02 22:06:23 +0700

    add map function

--- app/src/main/java/com/relieve/android/components/DiscoverItem.kt
+++ app/src/main/java/com/relieve/android/components/DiscoverItem.kt
@@ -5,12 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout.LayoutParams
 import androidx.core.view.setMargins
+import com.google.android.gms.maps.GoogleMap
 import com.relieve.android.R
 import com.relieve.android.rsux.helper.dpToPx
 import com.relieve.android.rsux.helper.secondToTimeText
 import com.relieve.android.rsux.base.Item
 import com.relieve.android.rsux.base.RelieveViewHolder
 import kotlinx.android.synthetic.main.view_disaster_news.view.*
+import com.google.android.gms.maps.OnMapReadyCallback
+
 
 class DiscoverItem(val latitude: Double,
                    val longitude: Double,
@@ -22,10 +25,22 @@ class DiscoverItem(val latitude: Double,
     override val viewType = DiscoverItem::class.java.hashCode()
 
     override fun createViewHolder(parent: ViewGroup): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_disaster_news, parent, false))
+        return ViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_disaster_news, parent, false))
+            .also {
+                it.setupMap()
+            }
     }
 
     class ViewHolder(val view: View) : RelieveViewHolder<DiscoverItem>(view) {
+        var gMap: GoogleMap? = null
+
+        private val mapReadyCallBack = OnMapReadyCallback { map -> gMap = map }
+
+        fun setupMap() {
+            view.mvDisasterMap.getMapAsync(mapReadyCallBack)
+        }
+
         override fun bind(data: DiscoverItem) {
             view.apply {
                 val width = if (data.shouldFillWidth) LayoutParams.MATCH_PARENT else 146.dpToPx()
